57939e38311a1f287e38179b9794902b01794f10
Ivanson-Estrada <jivanea1289@gmail.com>
2019-03-11 11:23:32 -0600

    app-example/

---
+++ app-example/src/test/java/com/datio/appdemo/AppTest.java
@@ -0,0 +1,20 @@
+package com.datio.appdemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
